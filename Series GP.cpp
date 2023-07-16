830b9987ef5118f0969425de0a7205101b3cba90
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-16 18:06:47 +0530

    Series GP.cpp

---
+++ Series GP.cpp
@@ -0,0 +1,36 @@
+//Code
+public:
+	 long long power(int r, int n){
+
+     int m=1000000007;
+
+     if(n==0){
+
+         return 1;
+
+     }
+
+     long long t = power(r, n/2)%m;
+
+     t=t*t%m;
+
+     if(n%2==0){
+
+         return (t)%m;
+
+     }
+
+     return (r*t)%m;
+
+ }
+   	int Nth_term(int a, int r, int n){
+   	    // Code here
+   	     int m=1000000007;
+
+ 
+
+        int x= (a*power(r,n-1)%m);
+
+        return x%m;
+   	}    
+};
